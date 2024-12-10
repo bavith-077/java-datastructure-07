da8d76a70f55602236fd0daf20e7963211045d80
Bavith Mariyan <bavithmariyan12@gmail.com>
2024-12-10 20:23:20 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,46 @@
+//program to perform linear search:
+import java.util.Scanner;
+public class linearsearch{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size: ");
+        int size = sc.nextInt();
+        System.out.println("Enter the "+size+" number :");
+        int[] num = new int[size];
+        for(int i =0;i<size;i++){
+            num[i]=sc.nextInt();
+        }
+        System.out.println("enter the target that need to be find in the array:");
+        int target = sc.nextInt();
+        int result = linearsearchh(num,target);
+        if(result!=-1){
+            System.out.println("the entered target in the index of :"+result);
+        }
+        else{
+            System.out.println("entered target is not there in the array:");
+        }
+
+    }
+    public static int linearsearchh(int[] num,int target){
+        for(int i =0;i<num.length;i++){
+            if(num[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+
+}
+/*output:
+Enter the array size: 
+5
+Enter the 5 number :
+15
+2
+56
+90
+34
+enter the target that need to be find in the array:
+56
+the entered target in the index of :2 */
