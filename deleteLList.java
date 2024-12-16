4711a2f8709a85e4407623aa62188ffa50d9a1ff
Bavith Mariyan <bavithmariyan12@gmail.com>
2024-12-16 18:56:55 +0530

    Add files via upload

---
+++ deleteLList.java
@@ -0,0 +1,67 @@
+//program to delete an element in a llinked list:
+ 
+ public class deleteLList {
+    public static void main(String args[]) {
+        LinkedList nums = new LinkedList();
+        nums.add(5);
+        nums.add(9);
+        nums.add(6);
+        nums.addFirst(1);
+        nums.delete(9);
+        nums.printValues();
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head = null;
+
+    // Method to add a new element to the linked list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode; // If the list is empty, make the new node the head
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next; // Traverse to the end of the list
+            }
+            current.next = newNode; // Link the new node at the end
+        }
+    }
+
+    // Method to print all elements of the linked list
+    public void printValues() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        newNode.next=head;
+        head=newNode;
+
+    }
+    public void delete(int data){
+        Node current =head;
+        while(current.next!=null&&current.next.data!=data){
+            current=current.next;
+        }
+        if(current.next!=null){
+            current.next=current.next.next;
+        }
+    }
+    
+}
